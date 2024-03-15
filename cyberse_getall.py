f2b269b944ce4532b02917692584a7ef0fc2961a
Petra Lynn Vanderhei <mvanderhei2@huskers.unl.edu>
2024-03-15 12:46:34 -0500

    Update cyberse_getall.py
    Parsing added

--- cyberse_getall.py
+++ cyberse_getall.py
@@ -1,13 +1,25 @@
+# Get a list of all Cyberse monsters
+# Written by Petra Lynn Vanderhei (mvanderhei2@huskers.unl.edu)
+#
+# Yugipedia's API and documentation is at https://yugipedia.com/api.php?action=help
+
 import requests
+import json
 
-url = 'https://yugipedia.com/api.php'
+url_yugipedia = 'https://yugipedia.com/api.php'
 
-query = 'action=ask&\
+query_yugipedia = 'action=ask&\
     format=json&\
     query=[[Concept%3ACG+monsters]][[Type%3A%3ACyberse]]|limit=500'
 
 headers = {'User-Agent': 'Cyberse-GetPriceBot/0.1 (https://github.com/Brunswick-Khorasan/Cyberse-GetPrice; mvanderhei2@huskers.unl.edu)' }
 
-response = requests.get(url, params=query, headers=headers)
+response = requests.get(url_yugipedia, params=query_yugipedia, headers=headers)
 
-print(response.content)
+json_list = json.loads(response.content)['query']['reuslts']
+
+string_list = ''
+
+for key in json_list:
+    name = json_list[key]['fulltext']
+    string_list += name + '\n'
